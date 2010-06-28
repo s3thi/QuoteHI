8b59976bd4ec9810ca1caf8d903e381795135db1
Ankur Sethi <get.me.ankur@gmail.com>
2010-06-28 18:31:42 +0530

    Added the simple_auth decorator.

--- quotehi/lib/helpers.py
+++ quotehi/lib/helpers.py
@@ -5,4 +5,14 @@ available to Controllers. This module is available to templates as 'h'.
 """
 # Import helpers as desired, or define your own, ie:
 #from webhelpers.html.tags import checkbox, password
+from pylons import session, url
 from pylons.controllers.util import redirect
+
+def simple_auth(controller):
+    def controller_auth(self):
+        if 'logged_in' in session and session['logged_in']:
+            controller()
+        else:
+            redirect(url(controller='admin', action='login'))
+    
+    return controller_auth
