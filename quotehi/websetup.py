a2ef94b30042cfc57fc96334a175b8b4663349c9
GeneralMaximus <get.me.ankur@gmail.com>
2010-07-02 20:19:49 +0530

    Create a default admin user during app setup.

--- quotehi/websetup.py
+++ quotehi/websetup.py
@@ -2,8 +2,11 @@
 import logging
 
 import pylons.test
-
 from quotehi.config.environment import load_environment
+import pymongo
+from getpass import getpass
+from random import random
+from hashlib import sha256
 
 log = logging.getLogger(__name__)
 
@@ -12,3 +15,19 @@ def setup_app(command, conf, vars):
     # Don't reload the app if it was loaded under the testing environment
     if not pylons.test.pylonsapp:
         load_environment(conf.global_conf, conf.local_conf)
+    
+    mongo_host = conf['mongo.host']
+    mongo_db = conf['mongo.db']
+    
+    try:
+        db_conn = pymongo.Connection(mongo_host)
+    except pymongo.errors.ConnectionFailure:
+        raise Exception('Could not connect to MongoDB.')
+    
+    db = db_conn[mongo_db]
+    email = raw_input('Enter admin email: ')
+    password = getpass('Enter password: ')
+    salt = int(random() * 10000000000)
+    encrypted_password = sha256(password + str(salt) + email).hexdigest()
+    db.users.insert({'email': email, 'password': encrypted_password,
+                     'salt': salt})
